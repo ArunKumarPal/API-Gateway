9457f455c382d66b5d2310f6f95e65442a006337
ArunKumarPal <arunpal692@gmail.com>
2025-09-01 13:49:58 +0530

    Update ErrorCode.java

--- src/main/java/com/arun/sample/gateway/constants/ErrorCode.java
+++ src/main/java/com/arun/sample/gateway/constants/ErrorCode.java
@@ -9,8 +9,8 @@ public enum ErrorCode {
     ERR_1001(HttpStatus.UNAUTHORIZED, "INVALID_TOKEN", "1001", "Unauthorized"),
     ERR_1002(HttpStatus.FORBIDDEN, "FORBIDDEN", "1002", "You do not have permission to access this API"),
     ERR_1003(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_ERROR", "1003", "Issue with internal services. Please try again after sometime"),
-    ERR_1004(HttpStatus.UNAUTHORIZED, "INVALID_TOKEN", "DIS-1004", "User is missing from Bearer Token"),
-    ERR_1005(HttpStatus.TOO_MANY_REQUESTS, "TOO_MANY_REQUESTS", "1004", "Rate limit exceeded.");
+    ERR_1004(HttpStatus.UNAUTHORIZED, "INVALID_TOKEN", "1004", "User is missing from Bearer Token"),
+    ERR_1005(HttpStatus.TOO_MANY_REQUESTS, "TOO_MANY_REQUESTS", "1005", "Rate limit exceeded.");
 
     private final HttpStatus statusCode;
     private final String status;
